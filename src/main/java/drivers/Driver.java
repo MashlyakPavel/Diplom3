7c8ec794f53c5249856cf703d5f6e7583aa07480
Pavel Mashlyak <mpainfin@gmail.com>
2024-11-28 21:36:51 +0300

    bug fixes 2

--- src/main/java/drivers/Driver.java
+++ src/main/java/drivers/Driver.java
@@ -1,5 +1,6 @@
-package driver;
+package drivers;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -33,4 +34,31 @@ public class Driver {
         options.setBinary(System.getenv("YANDEX_BROWSER_PATH"));
         return new ChromeDriver(options);
     }
-}
+
+        public static WebDriver getDriver(String browser) {
+            switch (browser.toLowerCase()) {
+                case "chrome":
+                    // Автоматическое определение версии браузера и загрузка драйвера
+                    //WebDriverManager.chromedriver().setup();
+                    //return new ChromeDriver();
+                    // Указанное определение версии браузера и загрузка драйвера
+                    WebDriverManager.chromedriver().browserVersion("131.0.6778.86").setup();
+                    return new ChromeDriver();
+                case "yandex":
+                    WebDriverManager.chromedriver().browserVersion("128").setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.setBinary("C:\\Users\\Anastasia\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe");
+                    return new ChromeDriver(options);
+                // Для других браузеров
+            /*case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                return new EdgeDriver();*/
+                default:
+                    throw new IllegalArgumentException("Unknown browser: " + browser);
+            }
+        }
+
+    }
